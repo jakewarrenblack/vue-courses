4c2371a95ce201badd5c40533ca75d32b65da669
Jake <jakewarrenblack01@gmail.com>
2021-12-07 23:56:38 +0000

    Overhaul to style of course view and course index. Now using card layout.

--- src/pages/courses/Show.vue
+++ src/pages/courses/Show.vue
@@ -1,28 +1,124 @@
 <template>
   <v-layout>
-    <v-col>
-      <v-flex>
-        <h1 class="m-auto text-center">{{ course.title }}</h1>
-      </v-flex>
-      <v-flex>
-        <v-container>
-          <hr />
-          <p>
-            {{ course.description }}
-          </p>
-          <p>
-            {{ course.points }}
-          </p>
-          <p>
-            {{ course.code }}
-          </p>
-          <v-btn class="mr-5" @click="deleteCourse(course.id)">Delete</v-btn>
-          <router-link :to="{ name: 'courses_edit', params: { id: course.id } }"
-            ><v-btn>Edit</v-btn>
-          </router-link>
-        </v-container>
-      </v-flex>
-    </v-col>
+    <v-row class="w-100">
+      <v-col class="m-auto mt-10" lg="5" sm="12">
+        <v-card class="mx-auto">
+          <v-card-text>
+            <v-chip>{{ course.code }}</v-chip>
+            <br />
+            <br />
+            <p class="text-h2 text--primary">{{ course.title }}</p>
+            <v-divider />
+            <br />
+            <div class="text--primary">
+              {{ course.description }}
+            </div>
+            <br />
+            <v-list-item class="pl-0" two-line>
+              <v-list-item-content>
+                <v-list-item-title>Course Level</v-list-item-title>
+                <v-list-item-subtitle>{{ course.level }}</v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+
+            <v-list-item class="pl-0" two-line>
+              <v-list-item-content>
+                <v-list-item-title>CAO Points</v-list-item-title>
+                <v-list-item-subtitle>{{ course.points }}</v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+          </v-card-text>
+          <v-card-actions>
+            <router-link
+              :to="{
+                name: 'courses_edit',
+                params: { id: course.id },
+              }"
+            >
+              <v-btn text color="deep-purple accent-4">Edit</v-btn>
+            </router-link>
+            <v-btn
+              @click="deleteCourse(course.id)"
+              text
+              color="deep-purple accent-4"
+              >Delete</v-btn
+            >
+          </v-card-actions>
+
+          <v-card-text>
+            <v-divider />
+            <p class="text-h4 text--primary">Enrolments for this course</p>
+          </v-card-text>
+          <v-row>
+            <v-col
+              v-for="enrolment in course.enrolments"
+              :key="enrolment.id"
+              sm="12"
+              lg="6"
+              class=""
+            >
+              <v-card class="m-4 pb-2">
+                <div class="d-flex flex-column justify-space-between">
+                  <v-card-title class="text-h5">{{
+                    enrolment.lecturer.name
+                  }}</v-card-title>
+
+                  <v-expansion-panels flat accordion>
+                    <v-expansion-panel>
+                      <v-expansion-panel-header
+                        >Details</v-expansion-panel-header
+                      >
+                      <v-expansion-panel-content>
+                        <v-list-item class="pl-0" two-line>
+                          <v-list-item-content>
+                            <v-list-item-title>Status</v-list-item-title>
+                            <v-list-item-subtitle>{{
+                              enrolment.status
+                            }}</v-list-item-subtitle>
+                          </v-list-item-content>
+                        </v-list-item>
+
+                        <v-list-item class="pl-0" two-line>
+                          <v-list-item-content>
+                            <v-list-item-title>Email</v-list-item-title>
+                            <v-list-item-subtitle>{{
+                              enrolment.lecturer.email
+                            }}</v-list-item-subtitle>
+                          </v-list-item-content>
+                        </v-list-item>
+
+                        <v-list-item class="pl-0" two-line>
+                          <v-list-item-content>
+                            <v-list-item-title>Phone</v-list-item-title>
+                            <v-list-item-subtitle>{{
+                              enrolment.lecturer.phone
+                            }}</v-list-item-subtitle>
+                          </v-list-item-content>
+                        </v-list-item>
+                      </v-expansion-panel-content>
+                    </v-expansion-panel>
+                  </v-expansion-panels>
+
+                  <v-card-actions>
+                    <router-link
+                      :to="{
+                        name: 'enrolments_show',
+                        params: { id: enrolment.id },
+                      }"
+                    >
+                      <v-btn class="ml-2 mt-5" outlined rounded small>
+                        View
+                      </v-btn>
+                    </router-link>
+                  </v-card-actions>
+                </div>
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-row></v-row>
   </v-layout>
 </template>
 
@@ -51,11 +147,8 @@ export default {
         })
         .then(async (response) => {
           console.log(response);
+          this.$router.push({ name: "courses_index" });
           alert("Success");
-          // this.courses = response.data.data;
-          //this.courses.pop()
-
-          //await this.getData().then((res) => (this.courses = res));
         })
         .catch((error) => console.log(error));
     },
